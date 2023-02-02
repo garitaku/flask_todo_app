fc65e47b03e25006c7ce5e9eca9df097ce55f719
garitaku <garitaku@gmail.com>
2023-02-02 17:13:17 +0900

    master

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db=SQLAlchemy(app)
+
+class Post(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(30),nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime,nullable=False)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/')
+def create():
+    return render_template('create.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
